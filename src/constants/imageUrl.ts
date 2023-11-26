0d91f3e79dc628ee5f45aa90b03c60b8bd2fcf62
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2023-11-26 11:34:23 +0700

    order user

--- src/constants/imageUrl.ts
+++ src/constants/imageUrl.ts
@@ -1,1 +1,1 @@
-export const logoUrl = 'https://res.cloudinary.com/diqyzhuc2/image/upload/v1694766653/hoaUi/logo_ssgtuy.png';
+export const logoUrl = 'https://res.cloudinary.com/diqyzhuc2/image/upload/v1700971559/logo_ssgtuy_1_dktoff.png';
