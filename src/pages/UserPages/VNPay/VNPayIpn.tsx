9e313c83857fbab184fd3bcd86bc30ffa936d9a2
DevidMonster <mrbat905@gmail.com>
2023-12-19 11:00:34 +0700

    fix vnpay

--- src/pages/UserPages/VNPay/VNPayIpn.tsx
+++ src/pages/UserPages/VNPay/VNPayIpn.tsx
@@ -5,7 +5,7 @@ import { baseUrl } from "../../../constants/baseUrl";
 import Loading from "../../../components/Loading/Loading";
 
 function VNPayIpn() {
-    const [sucess, setSuccess] = useState<boolean>(false)
+    const [sucess, setSuccess] = useState<string | null>(null)
     const [responseVNPay, setResponseVNPay] = useState<Record<string, string | number>>({})
     const location = useLocation();
     const fetchApi = async (data: Record<string, string | number>) => {
@@ -24,11 +24,11 @@ function VNPayIpn() {
             }
 
             const result = await response.json();
+            setResponseVNPay({ ...data, ...result })
             if (result.Message == 'success') {
-                setSuccess(true)
-                setResponseVNPay({ ...data, ...result })
+                setSuccess('success')
             } else {
-                setSuccess(false)
+                setSuccess('false')
             }
         } catch (error) {
             console.error('Error during API call:', error);
@@ -49,7 +49,7 @@ function VNPayIpn() {
         // Sử dụng đối tượng urlVariables theo cách bạn muốn
     }, [location]);
     return (
-        sucess ? (
+        sucess != null ? sucess == 'success' ? (
             <div className="min-h-screen flex items-center justify-center bg-gray-100 py-15">
                 <div className="bg-white p-6 rounded-lg shadow-md max-w-md w-full space-y-4">
                     <h1 className="text-2xl font-semibold text-gray-800">VNPAY Response</h1>
@@ -81,6 +81,26 @@ function VNPayIpn() {
 
                         <div>
                             <span className="font-semibold">Trạng thái giao dịch:</span> Thanh toán thành công
+                        </div>
+                    </div>
+
+                    <Link to="/orderComplete" className='p-2'>
+                        <Button danger type="primary" className="bg-blue-500 my-2">Tiếp tục</Button>
+                    </Link>
+                </div>
+            </div>
+        ) : (
+            <div className="min-h-screen flex items-center justify-center bg-gray-100 py-15">
+                <div className="bg-white p-6 rounded-lg shadow-md max-w-md w-full space-y-4">
+                    <h1 className="text-2xl font-semibold text-gray-800">VNPAY Response</h1>
+
+                    <div className="flex flex-col space-y-2">
+                        <div>
+                            <span className="font-semibold">Response Code:</span> {responseVNPay.RspCode}
+                        </div>
+
+                        <div>
+                            <span className="font-semibold ">Message:</span> <span className="text-green-500">{responseVNPay.Message}</span>
                         </div>
                     </div>
 
